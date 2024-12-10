229f28239f083c2b559e3687129850855cd2a2ac
ninetailsrabbit <danieldigitalspace@proton.me>
2024-12-10 13:57:13 +0000

    planet generator

---
+++ addons/ninetailsrabbit.pixel_space_background_generator/src/components/planet.gd
@@ -0,0 +1,11 @@
+extends Sprite2D
+
+func _ready():
+	material = material.duplicate()
+	
+	var light_x: float = randf_range(0.0, 1.0)
+	var light_y: float = randf_range(0.0, 1.0)
+	
+	material.set_shader_parameter("light_origin", Vector2(light_x, light_y))
+	material.set_shader_parameter("seed", randf_range(1.0, 10.0))
+	material.set_shader_parameter("pixels", int(scale.x * 100))
--- addons/ninetailsrabbit.pixel_space_background_generator/src/pixel_space_background_generator.gd
+++ addons/ninetailsrabbit.pixel_space_background_generator/src/pixel_space_background_generator.gd
@@ -2,11 +2,12 @@
 class_name PixelSpaceBackgroundGenerator extends Control
 
 const BackgroundStarScene: PackedScene = preload("res://addons/ninetailsrabbit.pixel_space_background_generator/src/components/background_star.tscn")
+const BackgroundPlanetScene: PackedScene = preload("res://addons/ninetailsrabbit.pixel_space_background_generator/src/components/planet.tscn")
 
 @export var tiled: bool = false
 @export var tile_offset: float = 10.0
 @export var reduce_background: bool = false
-@export var current_color_scheme: GradientTexture1D = preload("res://addons/ninetailsrabbit.pixel_space_background_generator/src/color_schemes/Colorscheme.tres")
+@export var current_color_scheme: GradientTexture1D = preload("res://addons/ninetailsrabbit.pixel_space_background_generator/src/color_schemes/desert.tres")
 @export_category("Stars")
 @export var display_stars: bool = true
 @export var auto_based_on_viewport_size: bool = true:
@@ -20,6 +21,14 @@ const BackgroundStarScene: PackedScene = preload("res://addons/ninetailsrabbit.p
 @export var min_amount_of_stars: int = 0
 ## When fixed amount of stars is zero this value will be used on a range creation
 @export var max_amount_of_stars: int = 0
+@export_category("Planets")
+@export var display_planets: bool = true
+## Set to zero to calculate the amount based on the viewport size
+@export var fixed_amount_of_planets: int = 0
+## When fixed amount of planets is zero this value will be used on a range creation
+@export var min_amount_of_planets: int = 0
+## When fixed amount of planets is zero this value will be used on a range creation
+@export var max_amount_of_planets: int = 1
 @export_category("Nebulae")
 @export var display_nebulaes: bool = true
 
@@ -41,14 +50,18 @@ func _validate_property(property: Dictionary):
 	
 
 func _ready() -> void:
+	
+	star_container.show_behind_parent = true
+	planet_container.show_behind_parent = true
+	
 	aspect = _calculate_aspect()
 	resized.connect(on_resized)
 	
 
 func generate_new() -> void:
 	_create_stars()
+	_create_planets()
 	_create_nebulae()
-	
 
 #region Stars
 func _create_stars() -> void:
@@ -105,12 +118,48 @@ func _create_nebulae() -> void:
 	nebulae.material.set_shader_parameter("should_tile", tiled)
 	nebulae.material.set_shader_parameter("reduce_background", reduce_background)
 	
-	
 #endregion
 
 #region Planets
+func _create_planets() -> void:
+	remove_planets()
+	
+	if not display_planets:
+		return
+		
+	var planets_amount: int = fixed_amount_of_planets
+	
+	if fixed_amount_of_planets == 0 and (min_amount_of_planets > 0 or max_amount_of_planets == 0):
+		planets_amount = randi_range(min_amount_of_planets, max_amount_of_planets)
+		
+		
+	for i in planets_amount:
+		_create_planet()
+	
+
+func _create_planet() -> void:
+	var min_size = min(size.x, size.y)
+	var planet_scale = Vector2.ONE * (randf_range(0.2, 0.7) * randf_range(0.5, 1.0) * min_size * 0.005)
+	var place_position: Vector2
+	
+	if (tiled):
+		var planet_offset: float = planet_scale.x * 100.0 * 0.5
+		place_position = Vector2(int(randf_range(planet_offset, size.x - planet_offset)), int(randf_range(planet_offset, size.y - planet_offset)))
+	else:
+		place_position = Vector2(int(randf_range(0, size.x)), int(randf_range(0, size.y)))
+	
+	var planet = BackgroundPlanetScene.instantiate()
+	planet.scale = planet_scale
+	planet.position = place_position
+	planet_container.add_child(planet)
+	planet_objects.append(planet)
 
 
+func remove_planets() -> void:
+	for planet in planet_objects:
+		planet.queue_free()
+			
+	planet_objects.clear()
 #endregion
 
 
